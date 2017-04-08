09a7f4850ce4888f6d2fd70de32ea783380cea48
Vishwas <gopinav5@gmail.com>
2017-04-08 16:54:29 +0530

    Add routing

--- ngApp/server.js
+++ ngApp/server.js
@@ -3,15 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const api = require('./server/routes/api');
-
 const port = 3000;
 
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json()); 
 
 app.use('/api', api);
 
--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
